ed93b80b313a36e85eaa7853867f7d62da6d2409
Rahul Koimattur <koimatturrahul704@gmail.com>
2021-10-13 15:59:15 +0530

    Login verification added to checkout page

---
+++ KitchenStory/src/app/services/auth.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService implements CanActivate {
+
+  constructor(private _http:HttpClient) { }
+  canActivate(): boolean {
+    if(this.isLoggedIn()) return true;
+    return false;
+  }
+
+  isLoggedIn(){
+    if(localStorage.getItem('isLoggedIn')=='true'){
+      return true;
+    }
+    return false;
+  }
+
+  Logout(){
+     localStorage.removeItem('isLoggedIn');
+     localStorage.removeItem('userId');
+  }
+
+  addUser(user:User):Observable<User>{
+    return this._http.post<User>("http://localhost:3000/users",user);
+  
+  }
+}
